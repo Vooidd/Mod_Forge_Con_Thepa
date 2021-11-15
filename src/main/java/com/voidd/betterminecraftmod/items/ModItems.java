097d16693088750f16d799b247dccff69c4d6351
Vooidd <marbleconch@gmail.com>
2021-11-15 16:53:01 +0100

    new feature

--- src/main/java/com/voidd/betterminecraftmod/items/ModItems.java
+++ src/main/java/com/voidd/betterminecraftmod/items/ModItems.java
@@ -1,6 +1,9 @@
 package com.voidd.betterminecraftmod.items;
 
 import com.voidd.betterminecraftmod.BetterMinecraftMod;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,13 +23,17 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERMINECRAFT_TAB)));
     public static final RegistryObject<Item> RUBY_CARROT = ITEMS.register("ruby_carrot",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERMINECRAFT_TAB).food
-                    (new FoodProperties.Builder().nutrition(6).saturationMod(2.4f).build())));
+                    (new FoodProperties.Builder().nutrition(6)
+                            .effect(()->new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 300), 1).saturationMod(2.4f).build())));
     public static final RegistryObject<Item> SAPPHIRE_CRYSTAL = ITEMS.register("sapphire_crystal",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERMINECRAFT_TAB)));
     public static final RegistryObject<Item> SAPPHIRE_NECKLACE = ITEMS.register("sapphire_necklace",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERMINECRAFT_TAB)));
     public static final RegistryObject<Item> SAPPHIRE_SHARD = ITEMS.register("sapphire_shard",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERMINECRAFT_TAB)));
+    public static final RegistryObject<Item> SAPPHIRE_CARROT = ITEMS.register("sapphire_carrot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTERMINECRAFT_TAB).food(new FoodProperties.Builder().nutrition(6)
+                    .effect(()->new MobEffectInstance(MobEffects.WATER_BREATHING,300),1).saturationMod(2.4f).build())));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
